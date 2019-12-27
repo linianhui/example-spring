a3762195fa8e8b4117183a2e4663238fae49b2d0
lnhcode <lnhcode@outlook.com>
2019-12-27 17:50:21 +0800

    Create HomeControllerTest.java

---
+++ src/test/java/lnh/api/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package lnh.api;
+
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+
+import org.junit.jupiter.api.Test;
+
+
+class HomeControllerTest extends HttpTest {
+
+  @Test
+  void test_get_home() {
+    http.get()
+        .uri("/")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody()
+        .consumeWith(document("get-home"));
+  }
+}
