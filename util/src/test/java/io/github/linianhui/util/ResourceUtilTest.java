dc6dfbe27c7cf76b3bb1efad35edaa08d8b526d3
lnh <lnhcode@outlook.com>
2020-01-01 01:43:18 +0800

    Remove \n

--- util/src/test/java/io/github/linianhui/util/ResourceUtilTest.java
+++ util/src/test/java/io/github/linianhui/util/ResourceUtilTest.java
@@ -8,6 +8,6 @@ public class ResourceUtilTest extends BaseTest {
     @Test
     public void test_readFileAsUtf8_should_file_all_content_when_file_is_exists() throws Exception {
         final String text = getResourceUtil().readFileAsUtf8("/readFileAsUtf8.txt");
-        Assertions.assertEquals("this is file content.\n2", text);
+        Assertions.assertEquals("this is file content.", text);
     }
 }
