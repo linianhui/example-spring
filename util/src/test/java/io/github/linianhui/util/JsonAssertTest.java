7d5de66fb4e4c7849c9797789f1c211d07862a55
lnh <lnhcode@outlook.com>
2019-12-31 23:18:18 +0800

    Update JsonAssertTest.java

--- util/src/test/java/io/github/linianhui/util/JsonAssertTest.java
+++ util/src/test/java/io/github/linianhui/util/JsonAssertTest.java
@@ -9,6 +9,10 @@ public class JsonAssertTest {
             "{\"id\":\"1\"}",
             "{\"id\":\"1\"}"
         );
+    }
+
+    @Test
+    public void test_assertEquals_should_pass_when_equals_with_ignoreFieldRegexPath() throws Exception {
         JsonAssert.assertEquals(
             "{\"id\":\"1\"}",
             "{\"id\":\"1\",\"name\":\"name\"}",
