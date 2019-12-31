6f48f4f19fd600737f5137b6e73f3a8bafeb8cb9
lnh <lnhcode@outlook.com>
2020-01-01 01:51:15 +0800

    Add FileController

---
+++ spring-example-service2/src/main/java/io/github/linianhui/springexample/service2/file/FileController.java
@@ -0,0 +1,43 @@
+package io.github.linianhui.springexample.service2.file;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/v1/file")
+public class FileController {
+
+    @PostMapping(path = "upload")
+    public Object uploadFile(
+        final @RequestPart(name = "id") String id,
+        final @RequestPart(name = "pojo") POJO pojo,
+        final @RequestPart(name = "files") MultipartFile[] multipartFiles
+    ) {
+        final Map<String, Object> map = new LinkedHashMap<>();
+        map.put("now", LocalDateTime.now(ZoneOffset.UTC).toString());
+        map.put("id", id);
+        map.put("pojo", pojo);
+        map.put("file_properties", Arrays.stream(multipartFiles).map(FileController::readFileProperty).toArray());
+        return map;
+    }
+
+    private static Object readFileProperty(
+        final MultipartFile multipartFile
+    ) {
+        final Map<String, Object> filePropertyMap = new LinkedHashMap<>();
+        filePropertyMap.put("name", multipartFile.getName());
+        filePropertyMap.put("file_name", multipartFile.getOriginalFilename());
+        filePropertyMap.put("length", multipartFile.getSize());
+        filePropertyMap.put("content_type", multipartFile.getContentType());
+        return filePropertyMap;
+    }
+}
---
+++ spring-example-service2/src/main/java/io/github/linianhui/springexample/service2/file/POJO.java
@@ -0,0 +1,13 @@
+package io.github.linianhui.springexample.service2.file;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class POJO {
+    private String id;
+    private String name;
+}
