cbad71cf89a6707b4e6af3cb003e7a7637c32823
lnh <lnhcode@outlook.com>
2020-01-05 21:00:31 +0800

    Service2 http response add http version

--- spring-example-service2/src/main/java/io/github/linianhui/springexample/service2/home/HomeController.java
+++ spring-example-service2/src/main/java/io/github/linianhui/springexample/service2/home/HomeController.java
@@ -1,6 +1,8 @@
 package io.github.linianhui.springexample.service2.home;
 
-import io.github.linianhui.springexample.service2.home.ExampleProperties;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,18 @@ public class HomeController {
     private ExampleProperties exampleProperties;
 
     @GetMapping
-    public Object getHome() {
-        return exampleProperties;
+    public Object getHome(final HttpServletRequest request) {
+        final Map<String, Object> map = new LinkedHashMap<>();
+        map.put("request", getRequest(request));
+        map.put("example_properties", exampleProperties);
+        return map;
     }
 
+    private static Map<String, Object> getRequest(
+        final HttpServletRequest request
+    ) {
+        final Map<String, Object> map = new LinkedHashMap<>();
+        map.put("protocol", request.getProtocol());
+        return map;
+    }
 }
