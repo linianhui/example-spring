957019a85a3bc17794350d1db6a463373eda8ed1
lnh <lnhcode@outlook.com>
2020-01-07 16:09:10 +0800

    Simplify http2 configuration

--- spring-example-service2/src/main/java/io/github/linianhui/springexample/service2/config/Http2Config.java
+++ spring-example-service2/src/main/java/io/github/linianhui/springexample/service2/config/Http2Config.java
@@ -1,13 +1,9 @@
 package io.github.linianhui.springexample.service2.config;
 
-
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer;
-import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
-import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,32 +17,14 @@ public class Http2Config {
 
     /**
      * 启用HTTP2
-     * 覆盖默认的UndertowServletWebServerFactory
-     * org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryConfiguration.EmbeddedUndertow
      */
     @Bean
-    public UndertowServletWebServerFactory undertowServletWebServerFactory(
-        final ObjectProvider<UndertowDeploymentInfoCustomizer> deploymentInfoCustomizers,
-        final ObjectProvider<UndertowBuilderCustomizer> builderCustomizers
-    ) {
-        final UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
-
-        deploymentInfoCustomizers
-            .orderedStream()
-            .forEach(factory::addDeploymentInfoCustomizers);
-
-        builderCustomizers
-            .orderedStream()
-            .forEach(factory::addBuilderCustomizers);
-
-        factory.addBuilderCustomizers(Http2Config::undertowEnableHttp2);
-
-        return factory;
-    }
-
-    private static Undertow.Builder undertowEnableHttp2(
-        final Undertow.Builder builder
-    ) {
-        return builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
+    public UndertowBuilderCustomizer undertowBuilderCustomizer() {
+        return new UndertowBuilderCustomizer() {
+            @Override
+            public void customize(Undertow.Builder builder) {
+                builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
+            }
+        };
     }
 }
